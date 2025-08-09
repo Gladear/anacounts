a24fb2d36d62f9699a16eb5d4c530be9bffa8ab3
Enzo <enzo.baldisserri@aworldforus.com>
2025-08-09 14:44:49 +0200

    chore(deps): update `:phoenix` to `1.8.0`

--- mix.exs
+++ mix.exs
@@ -75,7 +75,7 @@ defmodule App.MixProject do
       {:finch, "~> 0.18"},
 
       # Phoenix and server tooling
-      {:phoenix, "~> 1.7"},
+      {:phoenix, "~> 1.8"},
       {:phoenix_ecto, "~> 4.4"},
       {:phoenix_html, "~> 4.0"},
       {:phoenix_live_reload, "~> 1.4", only: :dev},
