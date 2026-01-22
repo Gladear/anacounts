6904925e11159d1680bad54428509ac3bb84257b
Enzo <enzo.baldisserri@outlook.fr>
2026-01-22 22:49:51 +0100

    chore(terraform): remove unused domain records (#430)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,21 +10,3 @@ variable "aws_region" {
   default     = "eu-west-3"
   description = "The region on which to deploy the AWS resources"
 }
-
-## Values retrieved from the Fly app
-
-variable "app_cname_name" {
-  description = "The name of the CNAME record to add to Route 53 DNS"
-}
-
-variable "app_cname_record" {
-  description = "The CNAME record to add to Route 53 DNS"
-}
-
-variable "app_a_record" {
-  description = "The A record to add to Route 53 DNS"
-}
-
-variable "app_aaaa_record" {
-  description = "The AAAA record to add to Route 53 DNS"
-}
